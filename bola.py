12e30fd76ebca65df9dddce992070b875eb4cab9
Erick Roberto <erps.snf24@uea.edu.br>
2024-11-24 18:20:47 -0400

    update classes

---
+++ bola.py
@@ -0,0 +1,14 @@
+
+class bola:
+
+    def __init__(self, cor, posicao_inicial):
+        self.cor = cor
+        self.posicao_inicial = posicao_inicial
+
+    def criar_bola():
+
+    def apagar_bola():
+
+    def movimento_bola():
+
+        
---
+++ raquete.py
@@ -0,0 +1,14 @@
+
+class raquete:
+
+    def __init__(self, cor, posicao_inicial):
+        self.cor = cor
+        self.posicao_incial = posicao_inicial
+    
+
+    def criar_raquete():
+
+    def apagar_raquete():
+
+    def movimento_raquete():
+        
